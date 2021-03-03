86a4107963395a322378aa405eff19bec8747a3d
Krzysztof Wardziak <krzysztof.wardziak97@gmail.com>
2021-03-03 11:58:08 +0100

    configured add edit blog cd

---
+++ CmsPhotoBlog/Models/BlogDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CmsPhotoBlog.Models
+{
+    [Table("tblBlogDetails")]
+    public class BlogDetails
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+        public string ImageName { get; set; }
+        public string Date { get; set; }
+        public string Name { get; set; }
+        public string ModifiedDate { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ CmsPhotoBlog/ViewModels/Blog/BlogDetailsVm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CmsPhotoBlog.Models;
+
+namespace CmsPhotoBlog.ViewModels.Blog
+{
+    public class BlogDetailsVm
+    {
+        public BlogDetailsVm()
+        {
+            
+        }
+
+        public BlogDetailsVm(BlogDetails row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Description = row.Description;
+            CategoryName = row.CategoryName;
+            CategoryId = row.CategoryId;
+            ImageName = row.ImageName;
+            Date = row.Date;
+            ModifiedDate = row.ModifiedDate;
+            Name = row.Name;
+        }
+
+        public int Id { get; set; }
+        [Display(Name = "Tytuł")]
+        [Required]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Display(Name = "Opis")]
+        [Required]
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+        public string ImageName { get; set; }
+        [Display(Name = "Data")]
+        public string Date { get; set; }
+        [Display(Name = "Nazwa")]
+        public string Name { get; set; }
+        [Display(Name= "Data modyfikacji")]
+        public string ModifiedDate { get; set; }
+
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IEnumerable<string> GalleryImages { get; set; }
+    }
+}
